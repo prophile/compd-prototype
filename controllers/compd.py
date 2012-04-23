1fe248f4246213792815d0292dc71c40c8a24ebd
Alastair Lynn <arplynn@gmail.com>
2012-04-23 01:00:10 +0100

    Jiggled around compd start order to make it work

--- controllers/compd.py
+++ controllers/compd.py
@@ -1,10 +1,12 @@
-from twisted.internet import reactor
+from twisted.internet import reactor, protocol, error
 
 from arena import ArenaController
 from screens import ScreenController
 from state import StateController
 
-if __name__ == "__main__":
+SERVER_COMMAND = '/usr/local/bin/redis-server'
+
+def bring_up_controllers():
     print "Bringing up state controller..."
     state = StateController()
     print "Bringing up arena controller..."
@@ -12,5 +14,30 @@ if __name__ == "__main__":
     print "Bringing up screen controller..."
     screens = ScreenController()
     print "compd started"
+
+def bring_up_redis():
+    class RedisProtocol(protocol.ProcessProtocol):
+        def processExited(self, status):
+            import sys
+            if isinstance(status.value, error.ProcessTerminated):
+                print "redis server killed: exit {0}".format(status.value.exitCode)
+            reactor.stop()
+
+        def received(self, data):
+            print data,
+
+        outReceived = received
+        errReceived = received
+
+        def connectionMade(self):
+            print "\tredis started."
+            reactor.callLater(2.8, bring_up_controllers)
+
+    process = RedisProtocol()
+    reactor.spawnProcess(process, SERVER_COMMAND, [SERVER_COMMAND, 'redis.conf'])
+
+if __name__ == "__main__":
+    print "Bringing up redis server..."
+    bring_up_redis()
     reactor.run()
 
